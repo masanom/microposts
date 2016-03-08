1563f8e6ff0592d5dee0865d30fc5d53069ab85f
masanom <logos@netplateau.jp>
2016-03-08 12:25:13 +0000

    retry create database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302124300) do
+ActiveRecord::Schema.define(version: 20160308113304) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160302124300) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "address"
+    t.text     "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
