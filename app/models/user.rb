fb229eb570855f980868f9d0ca17325d24c52709
masanom <logos@netplateau.jp>
2016-03-11 13:10:08 +0000

    challenge retry

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum:255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :address, :profile, length: { maximum: 255 }, on: :edit, on: :update
     
     validates :address, :profile, absence: true, on: :create
 end
