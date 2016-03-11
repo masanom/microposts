c10c57d978929a23f66fca8591ebb19b9d7163d7
masanom <logos@netplateau.jp>
2016-03-11 05:35:25 +0000

    confuse sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show, :edit, :update]
-  before_action :collect_user, only:[:edit, :update]
+  before_action :collect_user, only:[:show, :edit, :update]
   
   def show
   end
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   end
   
   def collect_user
-    user = User.find(params[:id])
-    redirect_to(root_url) if user != current_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) if @user != current_user
   end
 end
